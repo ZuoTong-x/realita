478004634fa5e112479b5ba49c2b21483c6840bb
xiangxuetong <360580347@qq.com>
2026-01-13 17:33:35 +0800

    fix:修复vite构建问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     base: "/",
     plugins: [
